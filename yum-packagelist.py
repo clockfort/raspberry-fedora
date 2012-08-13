591f847e2233ce334bff2a59370476d691e1a9de
Nathaniel Case <Qalthos@gmail.com>
2012-08-13 16:41:51 -0400

    Enclose file use in with syntax and simplify.

--- yum-packagelist.py
+++ yum-packagelist.py
@@ -5,9 +5,8 @@ import sys
 def packageList(groupname):
         return bs.find('id', text=groupname).parent.packagelist.stripped_strings
 
-handle = open('comps/comps-f17.xml.in', 'r')
-xml = handle.read()
-bs = BeautifulSoup(xml, "xml")
+with open('comps/comps-f17.xml.in') as xml:
+    bs = BeautifulSoup(xml, "xml")
 
 packages = set()
 for group in sys.argv[1:]:
